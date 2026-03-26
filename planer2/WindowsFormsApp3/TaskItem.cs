5632b44a9c7d50695d360f1528fdf6e3b8484869
niria <NiriaNiria@users.noreply.github.com>
2026-03-26 02:31:12 +0100

    dodanie projektu

---
+++ planer2/WindowsFormsApp3/TaskItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    internal class TaskItem
+    {
+        public string Name { get; set; }
+        public string Priority { get; set; }
+
+        public TaskItem(string name,string priority)
+        {
+            Name = name;
+            Priority = priority;
+        }
+        public override string ToString()
+        {
+            return Name + "(" + Priority + ")";
+        }
+
+    }
+}
